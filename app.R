a9a659e9a068880d1afd322c9082e04610e69d3c
Danilo <danibevi@colsubsidio.com>
2020-11-09 16:31:51 -0500

    Titulo del app fue retirado

--- app.R
+++ app.R
@@ -13,7 +13,6 @@ library(shiny)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
